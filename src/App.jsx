8ae7792e8cb449b2a2feeb1f1b9d4177f617884a
Muhammad Saad Akmal <120157311+saadakmal460@users.noreply.github.com>
2025-03-22 05:31:19 +0500

    Project starter

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='text-red-500 underline text-xl'>Tailwind working</div>
+    </>
+  )
+}
+
+export default App
