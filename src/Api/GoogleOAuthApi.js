e5a48e5cb67e92304059d7ddf604a9c8c3bc5865
Kazim68 <120156815+Kazim68@users.noreply.github.com>
2025-05-05 11:52:58 -0700

    Update GoogleOAuthApi.js

--- src/Api/GoogleOAuthApi.js
+++ src/Api/GoogleOAuthApi.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api",
+    //baseURL: "http://localhost:3000/api",
+    baseURL: "https://work-nexus-backend.vercel.app/api",
+    
 });
 
 export const googleAuth = (code) => api.get(`auth/google?code=${code}`);
